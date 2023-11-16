2347e89ab71175f4ff14cca9e852a241a6aa71e4
Deepu Raj <deepura1@in.ibm.com>
2023-11-16 17:01:51 +0530

    Updating changes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,54 +10,65 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     if request.form:
+        # print(f'Inside form request')
         num1 = request.form.get('first')
         num2 = request.form.get('second')
-        print(f'{num1} and {num2}')
-        return f'Sum of the two numbers = {num1+num2}'
+        # print(f'{num1} and {num2}')
+        ans = int(num1)+int(num2)
+        return jsonify('response = ans')
     elif request.json:
-        print(f'json data = {request.json}')
+        # print(f'json data = {request.json}')
         json = request.json
         num1 = json['first']
         num2 = json['second']
-        print(f'{num1} and {num2}')
-        return f'Sum of the two numbers = {num1+num2}'
+        # print(f'{num1} and {num2}')
+        ans = int(num1)+int(num2)
+        str = {"response": ans}
+        return jsonify(str)
     else:
         print(f'received other data types')
+    # return f'response: bad request'
     
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     if request.form:
+        # print(f'Inside form request')
         num1 = request.form.get('first')
         num2 = request.form.get('second')
-        print(f'{num1} and {num2}')
-        return f'Sum of the two numbers = {num1-num2}'
+        # print(f'{num1} and {num2}')
+        return f'response = {num1-num2}'
     elif request.json:
-        print(f'json data = {request.json}')
+        # print(f'json data = {request.json}')
         json = request.json
         num1 = json['first']
         num2 = json['second']
-        print(f'{num1} and {num2}')
-        return f'Difference of the two numbers = {num1-num2}'
+        ans = int(num1)-int(num2)
+        str = {"response": ans}
+        return jsonify(str)
     else:
         print(f'received other data types')
+    # return f'response: bad request'
 
 @app.route("/calculator/multiply", methods=['POST'])
 def multiply():
     if request.form:
+        # print(f'Inside request')
         num1 = request.form.get('first')
         num2 = request.form.get('second')
-        print(f'{num1} and {num2}')
-        return f'Sum of the two numbers = {num1*num2}'
+        # print(f'{num1} and {num2}')
+        return f'response = {num1*num2}'
     elif request.json:
-        print(f'json data = {request.json}')
+        # print(f'json data = {request.json}')
         json = request.json
         num1 = json['first']
         num2 = json['second']
-        print(f'{num1} and {num2}')
-        return f'Product of the two numbers = {num1*num2}'
+        ans = int(num1)*int(num2)
+        str = {"response": ans}
+        return jsonify(str)
     else:
         print(f'received other data types')
+    return f'response: bad request'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
