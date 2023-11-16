40c3a3cddd9ba6f38b097da4f0f6acb42db8098a
Deepu Raj <deepura1@in.ibm.com>
2023-11-16 17:28:37 +0530

    Another change

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World'
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
@@ -52,27 +52,27 @@ def subtract():
         print(f'received other data types')
     # return f'response: bad request'
 
-@app.route("/calculator/multiply", methods=['POST'])
-def multiply():
-    if request.form:
-        # print(f'Inside request')
-        num1 = request.form.get('first')
-        num2 = request.form.get('second')
-        # print(f'{num1} and {num2}')
-        ans = int(num1)*int(num2)
-        str = {"result": ans}
-        return jsonify(str)
-    elif request.json:
-        # print(f'json data = {request.json}')
-        json = request.json
-        num1 = json['first']
-        num2 = json['second']
-        ans = int(num1)*int(num2)
-        str = {"result": ans}
-        return jsonify(str)
-    else:
-        print(f'received other data types')
-    return f'response: bad request'
+# @app.route("/calculator/multiply", methods=['POST'])
+# def multiply():
+#     if request.form:
+#         # print(f'Inside request')
+#         num1 = request.form.get('first')
+#         num2 = request.form.get('second')
+#         # print(f'{num1} and {num2}')
+#         ans = int(num1)*int(num2)
+#         str = {"result": ans}
+#         return jsonify(str)
+#     elif request.json:
+#         # print(f'json data = {request.json}')
+#         json = request.json
+#         num1 = json['first']
+#         num2 = json['second']
+#         ans = int(num1)*int(num2)
+#         str = {"response": ans}
+#         return jsonify(str)
+#     else:
+#         print(f'received other data types')
+#     return f'response: bad request'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
